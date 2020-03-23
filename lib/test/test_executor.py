4f6045bdf6328e01a652a05fad711d5185e78e89
focus-sh <wangzhibin2013@126.com>
2020-03-23 09:35:22 +0800

    Logging feature study

---
+++ lib/test/test_executor.py
@@ -0,0 +1,28 @@
+import unittest
+import logging  # 引入logging模块
+import os.path
+import time
+from lib.environment import environment as env
+
+
+class TestExecutor(unittest.TestCase):
+
+    def test_logging(self):
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+        log_path = env.home() + '/logs/'
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+
+        log_name = log_path + rq + '.log'
+        logfile = log_name
+
+        fh = logging.FileHandler(logfile, mode='w')
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+        logger.info("hello world")
+
