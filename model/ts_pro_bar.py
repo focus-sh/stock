37010b7bb667dc1fac4826541d8f2d6c1db93fc8
focus-sh <wangzhibin2013@126.com>
2020-03-27 14:43:52 +0800

    Test pyecharts

--- model/ts_pro_bar.py
+++ model/ts_pro_bar.py
@@ -31,6 +31,12 @@ WHERE
 ORDER BY trade_date DESC
 LIMIT 1'''
 
+        self.select_all_valid_record_where_clause = '''
+WHERE
+    ts_code = %s
+        AND open IS NOT NULL
+ORDER BY trade_date'''
+
     def select_latest_record(self, ts_code):
         try:
             return pandas.create_data_frame_by_sql(
@@ -42,6 +48,14 @@ LIMIT 1'''
         except Exception:
             logging.exception(f'Query table{self.table_name} Failed')
             return None
+
+    def select_all_valid_record(self, ts_code):
+        return pandas.create_data_frame_by_sql(
+            sql=self.select_sql + self.select_all_valid_record_where_clause,
+            params=[ts_code],
+            subset=['ts_code', 'trade_date'],
+            keep='last'
+        )
 
 
 ts_pro_bar = TsProBar()
